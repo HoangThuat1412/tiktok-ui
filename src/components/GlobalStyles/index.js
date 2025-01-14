15ec181e0691ffbd94b457da3775fd3b2414fc7c
HoangThuat1412 <thuathvhd@gmail.com>
2025-01-14 14:21:42 +0700

    Cấu hình Router/Layout

--- src/components/GlobalStyles/index.js
+++ src/components/GlobalStyles/index.js
@@ -1,7 +1,5 @@
 import './GlobalStyles.scss'
-
-function GlobalStyles( { children} ) {
+function GlobalStyles({ children }) {
     return children
 }
-
 export default GlobalStyles
---
+++ src/pages/Upload/index.js
@@ -0,0 +1,7 @@
+import Header from "~/components/GlobalStyles/Layout/DefaultLayout/Header";
+
+function Upload() {
+    return <h2> Upload page </h2>
+}
+
+export default Upload;
