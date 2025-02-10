d0c173e6b0a8eacb579da6f50c844d4d5f550834
HoangThuat1412 <thuathvhd@gmail.com>
2025-02-11 01:16:41 +0700

    Xây dựng logic phần Header p1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,13 +2,14 @@ import classNames from "classnames/bind";
 import styles from "./AccountIteam.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import Image from "../Image";
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
     return (
         <divb className={cx('wrapper')}>
-            <img className={cx("avatar")} src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/118441977edc639baf728fd892d500b3.jpeg?lk3s=30310797&nonce=10141&refresh_token=725c67446116c78cd2aadf2ef971ea1e&x-expires=1737363600&x-signature=bWYWFhQnVbrwCVion0vRSyC0hII%3D&shp=30310797&shcp=-"
+            <Image className={cx("avatar")} src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/118441977edc639baf728fd892d500b3.jpeg?lk3s=30310797&nonce=10141&refresh_token=725c67446116c78cd2aadf2ef971ea1e&x-expires=1737363600&x-signature=bWYWFhQnVbrwCVion0vRSyC0hII%3D&shp=30310797&shcp=-"
                 alt="Hoahnanasi" />
             <div className={cx('infor')}>
                 <h4 className={cx('name')}>
