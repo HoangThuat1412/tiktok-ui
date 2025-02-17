abb3a04faf1badff4211500ea53f3c6eb62c584c
HoangThuat1412 <thuathvhd@gmail.com>
2025-02-17 15:37:36 +0700

    Sửa lỗi và hoàn thiện phần Header p2

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -40,10 +40,15 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
                     <PopperWrapper className={cx('menu-popper')}>
-                        {history.length > 1 && <Header title="Language" onBack={() => {
-                            setHistory(prev => prev.slice(0, prev.length - 1));
-                        }} />}
-                        {renderItems()}
+                        {history.length > 1 && (
+                            <Header
+                                title="Language"
+                                onBack={() => {
+                                    setHistory(prev => prev.slice(0, prev.length - 1));
+                                }}
+                            />
+                        )}
+                        <div className={cx('menu-body')}>{renderItems()}</div>
                     </PopperWrapper>
                 </div>
             )}
